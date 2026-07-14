63c9e809d8243f7bc5e5509e8e8706f957da0ef9
mehdiprgm <zendevmehdi@gmail.com>
2026-07-14 16:34:14 +0330

    gives you exact terminal window size

---
+++ src/com/zen/lib/terminal/driver/TerminalWindowSize.java
@@ -0,0 +1,13 @@
+package com.zen.lib.terminal.driver;
+
+/**
+ * Represents the size of a terminal window.
+ *
+ * <p>This record stores the current width and height of the terminal.</p>
+ *
+ * @author Mehdi Lavasani (zenDEv2)
+ * @version 1.0
+ * @since 21
+ */
+public record TerminalWindowSize(int width, int height) {
+}
